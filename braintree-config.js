c5956de06883489e3570328cead9b93722fd932d
D3ADCRAFT <kcatlin100@yahoo.com>
2026-05-28 21:56:02 -0500

    Update braintree-config.js

--- braintree-config.js
+++ braintree-config.js
@@ -6,6 +6,6 @@ const PROJECT_ID = "good-knight-boutique";
 const BASE = `https://${REGION}-${PROJECT_ID}.cloudfunctions.net`;
 
 export const braintreeConfig = {
-  clientTokenUrl: `${BASE}/braintreeClientToken`,
-  checkoutUrl: `${BASE}/braintreeCheckout`
+  clientTokenUrl: "https://us-central1-good-knight-boutique.cloudfunctions.net/braintreeClientToken",
+  checkoutUrl: "https://us-central1-good-knight-boutique.cloudfunctions.net/braintreeCheckout"
 };
